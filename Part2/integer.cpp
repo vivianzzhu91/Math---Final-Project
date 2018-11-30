5d1dd7b0c15ff07fd714e7ed1fac7027df2ed2c6
Yuyang Huang <yuyangh@usc.edu>
2018-11-29 22:44:19 -0800

    change to correct

--- Part2/integer.cpp
+++ Part2/integer.cpp
@@ -8,7 +8,7 @@
 #include <cmath>
 using namespace std;
 
-string ConvertBinary(int n)
+string ConvertBinary(long long n)
 {
     string r;
     while(n!=0) {
@@ -23,15 +23,30 @@ string ConvertBinary(int n)
     return r;
 }
 
-int main(){
-	int power=4;
+void output(int power){
 	string filename="output_2^"+to_string(power)+".txt";
-    ofstream outputfile(filename);
-    outputfile << '0';
-    for (int i =0;i<=pow(2,power);i++) {
-	    string line = ConvertBinary(i);
-	    std::reverse(line.begin(), line.end());
-        outputfile << line;
-    }
+	ofstream outputfile(filename);
+	outputfile << '0';
+	for (int i =0;i<=pow(2,power);i++) {
+		string line = ConvertBinary(i);
+		std::reverse(line.begin(), line.end());
+		outputfile << line;
+	}
+}
+
+int main(){
+	for (int i = 0; i < 18; ++i) {
+		output(i);
+	}
+	
+	// int power=4;
+	// string filename="output_2^"+to_string(power)+".txt";
+    // ofstream outputfile(filename);
+    // outputfile << '0';
+    // for (int i =0;i<=pow(2,power);i++) {
+	//     string line = ConvertBinary(i);
+	//     std::reverse(line.begin(), line.end());
+    //     outputfile << line;
+    // }
     return 0;
 }
